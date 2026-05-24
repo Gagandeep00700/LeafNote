3b4032fd6523da8bbd4dab7c5994fda1fda19b5e
Gagandeep00700 <116875219+Gagandeep00700@users.noreply.github.com>
2026-05-24 18:52:15 +0530

    Database connection fixed

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -5,11 +5,14 @@ dotenv.config();
 
 const connectDB= async()=>{
     try{
-        const conn= await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("Database Connected")
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(error)
     {
-        console.error("Error connecting database")
+        console.error("Error connecting database",error)
         process.exit(1);
     }
 }
